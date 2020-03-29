3821a7b91c73e4ac05351147ae24a93d25cc1b6c
Aleksandr Bobrovnikov <freeman.exe@mail.ru>
2020-03-29 13:26:15 +0300

    finish config for deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   router: {
-    base: "/"
+    base: "/15-game/"
   },
 
   head: {
@@ -10,7 +10,7 @@ module.exports = {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "Nuxt.js project" }
     ],
-    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/15-game/favicon.png" }]
   },
 
   loading: { color: "#3B8070" },
